a06f54a031cabce01378c25ceb3cc90e6281b553
Vigo Vlugt <vigovlugt@gmail.com>
2021-03-19 21:14:14 +0100

    Add env variables

--- src/api/ItemApi.ts
+++ src/api/ItemApi.ts
@@ -2,14 +2,14 @@ import Item from "../models/Item";
 
 export default class ItemApi {
     static async getAllItems(){
-        const res = await fetch("http://localhost:5001/api/items");
+        const res = await fetch(process.env.API_URL + "/api/items");
         const items = await res.json();
 
         return items;
     }
 
     static async getItem(id){
-        const res = await fetch(`http://localhost:5001/api/items/${id}`);
+        const res = await fetch(`${process.env.API_URL}/api/items/${id}`);
         const item = await res.json();
 
         return item;
--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,4 +1,4 @@
-import classNames from "classNames";
+import classNames from "classnames";
 import Link from "next/link";
 import ListItem from "./icons/ListIcon";
 import GridIcon from "./icons/GridIcon";
