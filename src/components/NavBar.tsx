d3dd4d09c4c15bba9d72a9a9d8cad080e43d4d44
Vigo Vlugt <vigovlugt@gmail.com>
2021-04-19 21:18:46 +0200

    Fix shrinking navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./layout/SearchBar";
 
 export default function NavBar({ title }) {
   return (
-    <nav className="w-full p-3 bg-white border-b border-gray-200 flex justify-between h-[65px]">
+    <nav className="w-full p-3 bg-white border-b border-gray-200 flex justify-between h-[65px] flex-shrink-0">
       <div className="w-px flex items-center">
         <h1 className="text-2xl font-header font-medium whitespace-nowrap">
           {title}&nbsp;
