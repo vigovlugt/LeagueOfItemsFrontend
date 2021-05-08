0810d0785b250f0c46eccaa738196d69fc0874e9
Vigo Vlugt <vigovlugt@gmail.com>
2021-05-08 13:24:20 +0200

    Mobile fixes
    Former-commit-id: 0de71bdd55a14437fb53538ff21b59ef8940126f

--- src/components/champions/ChampionRoles.tsx
+++ src/components/champions/ChampionRoles.tsx
@@ -2,7 +2,7 @@ import RoleCard from "../roles/RoleCard";
 
 export default function ChampionRoles({ roleStats }) {
   return (
-    <div className="flex flex-row space-x-3 overflow-x-auto">
+    <div className="flex flex-row space-x-3 pb-2 overflow-x-auto lg:pb-0">
       {roleStats.map((stats) => (
         <RoleCard key={stats.role} {...stats} />
       ))}
