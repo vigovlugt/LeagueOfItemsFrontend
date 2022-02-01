c345cf13fa55b6aa5e9fdd05bcc6bad127713128
Vigo Vlugt <vigovlugt@gmail.com>
2022-02-01 18:02:47 +0100

    Add role winrate changes to PatchNotesChange

--- src/components/home/CategoryPreviews.tsx
+++ src/components/home/CategoryPreviews.tsx
@@ -1,60 +1,87 @@
 import Link from "next/link";
 import styles from "./CategoryPreviews.module.css";
+import { ArrowSmDownIcon } from "@heroicons/react/solid";
 
 export default function CategoryPreviews() {
+  const scrollToId = (id, e) => {
+    e.preventDefault();
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className="mb-16 flex w-full flex-col items-stretch sm:flex-row sm:space-x-4">
-      <Link href="/items" passHref>
+    <div className="mb-16">
+      <div className="mb-4 flex w-full flex-col items-stretch sm:flex-row sm:space-x-4">
+        <Link href="/items" passHref>
+          <a
+            className="group relative w-full overflow-hidden rounded-lg bg-white dark:bg-black"
+            style={{
+              aspectRatio: "1 / 1",
+            }}
+          >
+            <img
+              src="/images/items/shopkeeper.webp"
+              alt=""
+              className={`${styles.categoryImageItems} absolute inset-0 h-full`}
+            />
+            <h2 className="pointer-events-none absolute left-4 bottom-9 font-header text-4xl group-hover:text-white">
+              Items
+            </h2>
+            <span className="pointer-events-none absolute left-4 bottom-4 font-header text-sm text-gray-500 group-hover:text-white">
+              View all
+            </span>
+          </a>
+        </Link>
+        <div className="flex w-full flex-col space-y-4">
+          <Link href="/runes" passHref>
+            <a className="group relative mt-4 h-full min-h-[300px] overflow-hidden rounded-lg bg-white p-4 dark:bg-dark sm:mt-0 sm:min-h-0">
+              <img
+                src="/images/runes/runes-reforged.webp"
+                alt=""
+                className={`${styles.categoryImageRunes} absolute inset-0 h-full w-full`}
+              />
+              <h2 className="pointer-events-none absolute bottom-9 font-header text-2xl group-hover:text-white">
+                Runes
+              </h2>
+              <span className="pointer-events-none absolute bottom-4 font-header text-sm text-gray-500 group-hover:text-white">
+                View all
+              </span>
+            </a>
+          </Link>
+          <Link href="/champions" passHref>
+            <a className="group relative mt-4 h-full min-h-[300px] overflow-hidden rounded-lg bg-white p-4 dark:bg-dark sm:mt-0 sm:min-h-0">
+              <img
+                src="/images/champions/champions.webp"
+                alt=""
+                className={`${styles.categoryImageChampions} absolute inset-0 h-full w-full `}
+              />
+              <h2 className="pointer-events-none absolute bottom-9 font-header text-2xl group-hover:text-white">
+                Champions
+              </h2>
+              <span className="pointer-events-none absolute bottom-4 font-header text-sm text-gray-500 group-hover:text-white">
+                View all
+              </span>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
         <a
-          className="group relative w-full overflow-hidden rounded-lg bg-white p-4 dark:bg-black"
-          style={{
-            aspectRatio: "1 / 1",
-          }}
+          href="#patch-overview"
+          className="flex w-full items-center justify-center rounded-lg bg-white p-2 text-lg shadow dark:bg-gray-900 dark:text-gray-50"
+          onClick={(e) => scrollToId("patch-overview", e)}
         >
-          <img
-            src="/images/items/shopkeeper.webp"
-            alt=""
-            className={`${styles.categoryImageItems} absolute inset-0 h-full`}
-          />
-          <h2 className="pointer-events-none absolute bottom-9 font-header text-4xl group-hover:text-white">
-            Items
-          </h2>
-          <span className="pointer-events-none absolute bottom-4 font-header text-sm text-gray-500 group-hover:text-white">
-            View all
-          </span>
+          <h2 className="font-header text-lg">Patch overview</h2>
+          <ArrowSmDownIcon className="inline w-8 text-gray-600 dark:text-gray-400" />
         </a>
-      </Link>
-      <div className="flex w-full flex-col space-y-4">
-        <Link href="/runes" passHref>
-          <a className="group relative mt-4 h-full min-h-[300px] overflow-hidden rounded-lg bg-white p-4 dark:bg-dark sm:mt-0 sm:min-h-0">
-            <img
-              src="/images/runes/runes-reforged.webp"
-              alt=""
-              className={`${styles.categoryImageRunes} absolute inset-0 h-full`}
-            />
-            <h2 className="pointer-events-none absolute bottom-9 font-header text-2xl group-hover:text-white">
-              Runes
-            </h2>
-            <span className="pointer-events-none absolute bottom-4  font-header text-sm text-gray-500 group-hover:text-white">
-              View all
-            </span>
-          </a>
-        </Link>
-        <Link href="/champions" passHref>
-          <a className="group relative mt-4 h-full min-h-[300px] overflow-hidden rounded-lg bg-white p-4 dark:bg-dark sm:mt-0 sm:min-h-0">
-            <img
-              src="/images/champions/champions.webp"
-              alt=""
-              className={`${styles.categoryImageChampions} absolute inset-0 h-full`}
-            />
-            <h2 className="pointer-events-none absolute bottom-9 font-header text-2xl group-hover:text-white">
-              Champions
-            </h2>
-            <span className="pointer-events-none absolute bottom-4 font-header text-sm text-gray-500 group-hover:text-white">
-              View all
-            </span>
-          </a>
-        </Link>
+
+        <a
+          href="#patch-rundown"
+          className="flex w-full items-center justify-center rounded-lg bg-white p-2 text-lg shadow dark:bg-gray-900 dark:text-gray-50"
+          onClick={(e) => scrollToId("patch-rundown", e)}
+        >
+          <h2 className="font-header text-lg">Patch notes</h2>
+          <ArrowSmDownIcon className="inline w-8 text-gray-600 dark:text-gray-400" />
+        </a>
       </div>
     </div>
   );
--- src/components/home/HomeSidebar.tsx
+++ src/components/home/HomeSidebar.tsx
@@ -6,34 +6,9 @@ import { ArrowSmDownIcon, ArrowSmRightIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 
 export default function HomeSidebar({ pickrateBuilds, winrateBuilds }) {
-  const scrollToId = (id, e) => {
-    e.preventDefault();
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <div className="flex flex-col">
-      <a
-        href="#patch-overview"
-        className="hidden items-center justify-center rounded bg-white p-2 text-lg shadow dark:bg-gray-900 dark:text-gray-50 2xl:flex"
-        onClick={(e) => scrollToId("patch-overview", e)}
-      >
-        <h2 className="font-header text-lg">Patch overview</h2>
-        <ArrowSmDownIcon className="inline w-8 text-gray-600 dark:text-gray-400" />
-      </a>
-
-      <a
-        href="#patch-rundown"
-        className="mt-4 hidden items-center justify-center rounded bg-white p-2 text-lg shadow dark:bg-gray-900 dark:text-gray-50 2xl:flex"
-        onClick={(e) => scrollToId("patch-rundown", e)}
-      >
-        <h2 className="font-header text-lg">Patch notes</h2>
-        <ArrowSmDownIcon className="inline w-8 text-gray-600 dark:text-gray-400" />
-      </a>
-
-      <h3 className="mt-4 mb-1 font-header text-xl">
-        Biggest pickrate increases
-      </h3>
+      <h3 className="mb-1 font-header text-xl">Biggest pickrate increases</h3>
       <BuildsTable builds={pickrateBuilds} type="pickrate" size="sm" />
 
       <h3 className="my-1 mt-4 font-header text-xl">
