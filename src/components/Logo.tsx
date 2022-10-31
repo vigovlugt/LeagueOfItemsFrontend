e214526b00ad5441b5fe522b1d4491f5df5c611b
Vigo Vlugt <vigovlugt@gmail.com>
2022-10-31 10:13:21 +0100

    Update nextjs to v13

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Link href={"/"} passHref>
-      <a>
-        <h2 className="mb-8 font-header text-[1.65rem] font-medium text-black dark:text-white">
-          LEAGUE OF
-          <span className="block text-5xl">ITEMS</span>
-        </h2>
-      </a>
-    </Link>
+    (<Link href={"/"}>
+
+      <h2 className="mb-8 font-header text-[1.65rem] font-medium text-black dark:text-white">
+        LEAGUE OF
+        <span className="block text-5xl">ITEMS</span>
+      </h2>
+
+    </Link>)
   );
 }
