af87db2e4cfe18cc2108b55ac14fd22a5f05cce0
Vigo Vlugt <vigovlugt@gmail.com>
2026-01-11 16:31:29 +0100

    Simplify patch notes URL generation

--- src/utils/patch.ts
+++ src/utils/patch.ts
@@ -2,8 +2,6 @@ export function getPatchNotesUrl(patch: string) {
     const [major, minor] = patch.split(".").map(Number);
 
     const season = major + 10;
-    const seasonSplit = Math.floor((minor - 1) / 8) + 1;
-    const splitPatch = ((minor - 1) % 8) + 1;
 
-    return `https://www.leagueoflegends.com/en-us/news/game-updates/patch-${season}-s${seasonSplit}-${splitPatch}-notes/`;
+    return `https://www.leagueoflegends.com/en-us/news/game-updates/patch-${season}-${minor}-notes/`;
 }
