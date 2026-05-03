859b8b9ad8356c4001c6bb1d981f7dbee49f1ffb
vigovlugt <vigovlugt@gmail.com>
2026-05-03 15:20:08 +0200

    Fix patch url

--- src/utils/patch.ts
+++ src/utils/patch.ts
@@ -1,7 +1,5 @@
 export function getPatchNotesUrl(patch: string) {
     const [major, minor] = patch.split(".").map(Number);
 
-    const season = major + 10;
-
-    return `https://www.leagueoflegends.com/en-us/news/game-updates/league-of-legends-patch-${season}-${minor}-notes/`;
+    return `https://www.leagueoflegends.com/en-us/news/game-updates/league-of-legends-patch-${major}-${minor}-notes/`;
 }
